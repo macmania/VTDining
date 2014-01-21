ec64ec0bd25315a623b0a93fdfe72b0f69e66081
jojofabe <jojofabe@vt.edu>
2014-01-21 11:29:14 -0500

    Initial Commit

---
+++ VTDiningMenuTests/VTDiningMenuTests.m
@@ -0,0 +1,34 @@
+//
+//  VTDiningMenuTests.m
+//  VTDiningMenuTests
+//
+//  Created by Jouella Fabe on 1/21/14.
+//  Copyright (c) 2014 Jouella Fabe. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VTDiningMenuTests : XCTestCase
+
+@end
+
+@implementation VTDiningMenuTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
