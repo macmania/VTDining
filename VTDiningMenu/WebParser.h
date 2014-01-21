06c7fba3059db8fc329dc2b16ac0997695e32267
jojofabe <jojofabe@vt.edu>
2014-01-21 15:45:13 -0500

    added a parser controller that manages in parsing xml or html data and send this data in a better format

---
+++ VTDiningMenu/WebParser.h
@@ -0,0 +1,20 @@
+//
+//  WebParser.h
+//  VTDiningMenu
+//
+//
+//  Temporary parser to hold an object that has fields name and value
+//
+//  Created by Jouella Fabe on 1/21/14.
+//  Copyright (c) 2014 Jouella Fabe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WebParser : NSObject{
+    
+}
+
+//- (id) getNutritionalData:N 
+
+@end
---
+++ VTDiningMenu/WebParser.m
@@ -0,0 +1,15 @@
+//
+//  WebParser.m
+//  VTDiningMenu
+//
+//  Temporary parser for the nutritional facts
+//
+//  Created by Jouella Fabe on 1/21/14.
+//  Copyright (c) 2014 Jouella Fabe. All rights reserved.
+//
+
+#import "WebParser.h"
+
+@implementation WebParser
+
+@end
